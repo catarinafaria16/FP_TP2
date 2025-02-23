1b027e59708092a11abb09705da126417b8f8a9a
catarinafaria16 <up202405128@up.pt>
2025-02-23 11:01:39 +0000

    Third commit

---
+++ src/Cilindro.java
@@ -0,0 +1,30 @@
+public class Cilindro {
+        private double raio;
+        private double altura;
+
+        public double getRaio() {
+            return raio;
+        }
+
+        public void setRaio(double raio) {
+            this.raio = raio;
+        }
+
+        public double getAltura() {
+            return altura;
+        }
+
+        public void setAltura(double altura) {
+            this.altura = altura;
+        }
+
+        public double calcularVolume() {
+            return Math.PI * Math.pow(raio, 2) * altura;
+        }
+
+        public double calcularArea() {
+            return 2 * Math.PI * raio * (raio + altura);
+        }
+
+    }
+
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        int x=0;
-    }
-}
+                Cilindro cilindro = new Cilindro();
+                cilindro.setRaio(2);
+                cilindro.setAltura(3);
+
+                Cilindro cil2=new Cilindro();
+                cil2.setRaio(3);
+                cil2.setAltura(5);
+
+                System.out.println("\nVolume: " + cilindro.calcularVolume());
+                System.out.println("Área: " + cilindro.calcularArea());
+
+                System.out.println("\nVolume: " + cil2.calcularVolume());
+                System.out.println("Área: " + cil2.calcularArea());
+
+
+            }
+
+        }
